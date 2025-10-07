cc59fdb41b10672a460eb1c4c2fda31055fb1f7e
miramsalp <Thanapat_petch@hotmail.com>
2025-10-07 22:09:20 +0700

    test create in sql

--- backend/src/utils/prisma.ts
+++ backend/src/utils/prisma.ts
@@ -2,10 +2,31 @@ import { PrismaClient } from '@prisma/client';
 
 export const prisma = new PrismaClient();
 
+// const testDb = async () => {
+//     let testUser = await prisma.user.findUnique({
+//         where: {
+//             email: 'test@example.com'
+//         }
+//     });
+
+//     if (!testUser) {
+//         testUser = await prisma.user.create({
+//             data: {
+//                 email: 'test@example.com',
+//                 password: 'password123'
+//             }
+//         });
+//         console.log('Test user created:', testUser.email);
+//     } else {
+//         console.log('Test user already exists:', testUser.email);
+//     }
+// }
+
 export const initializePrisma = async () => {
     try {
         await prisma.$connect();
         console.log('Prisma connected');
+        // await testDb();
     } catch (error) {
         console.error('Error connecting to Prisma:', error);
     }
