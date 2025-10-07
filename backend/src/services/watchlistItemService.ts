4dd8373984175ee2ffdba05b19a4e119a6491d5c
miramsalp <Thanapat_petch@hotmail.com>
2025-10-07 23:38:00 +0700

    add watchlistItem service

---
+++ backend/src/services/watchlistItemService.ts
@@ -0,0 +1,46 @@
+import { prisma } from "../utils/prisma.js";
+
+export const getWatchlistItem = async (userId: number) => {
+    const watchlist = await prisma.watchlistItem.findMany({
+        where: {
+            userId: userId
+        }
+    });
+    return watchlist;
+}
+
+export const addWatchlistItem = async (userId: number, symbol: string) => {
+    // stock mostly in any app will be uppercase
+    symbol = symbol.toUpperCase();
+    const existingItem = await prisma.watchlistItem.findFirst({
+        where: {
+            userId: userId,
+            symbol: symbol
+        }
+    });
+    if (existingItem) throw new Error("Item already in watchlist");
+    const newItem = await prisma.watchlistItem.create({
+        data: {
+            userId: userId,
+            symbol: symbol
+        }
+    });
+    return newItem;
+}
+
+export const removeWatchlistItem = async (userId: number, symbol: string) => {
+    symbol = symbol.toUpperCase();
+    const existingItem = await prisma.watchlistItem.findFirst({
+        where: {
+            userId: userId,
+            symbol: symbol
+        }
+    });
+    if (!existingItem) throw new Error("Item not found in watchlist");
+    const deletedItem = await prisma.watchlistItem.delete({
+        where: {
+            id: existingItem.id
+        }
+    });
+    return deletedItem;
+}
